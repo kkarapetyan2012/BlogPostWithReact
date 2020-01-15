26af60aaf5466a63fa23ac50201028726b8dea36
kkarapetyan2012 <kkarapetyan2012@gmail.com>
2020-01-15 18:33:42 +0400

    Updates

---
+++ precache-manifest.af082b73a8b412e210b5d65d7f3ae54a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4ac62ff97d08792c5c171873848d24",
+    "url": "/BlogPostWithReact/index.html"
+  },
+  {
+    "revision": "3b29c91888ee66e83f84",
+    "url": "/BlogPostWithReact/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "ae1a2f072b392555b7aa",
+    "url": "/BlogPostWithReact/static/css/main.d7b34e49.chunk.css"
+  },
+  {
+    "revision": "3b29c91888ee66e83f84",
+    "url": "/BlogPostWithReact/static/js/2.284a7b9c.chunk.js"
+  },
+  {
+    "revision": "ae1a2f072b392555b7aa",
+    "url": "/BlogPostWithReact/static/js/main.b28a7ace.chunk.js"
+  },
+  {
+    "revision": "a901da807dc9b9db3c85",
+    "url": "/BlogPostWithReact/static/js/runtime-main.c0455e8f.js"
+  }
+]);
